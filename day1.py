387ad8fcda84c3500ff267639a55d4ed8e06f888
Rashid004 <rashidansari3868038@gmail.com>
2026-06-06 20:15:09 +0530

    feat: start learning python

---
+++ day1.py
@@ -0,0 +1,11 @@
+print("Hello world!")
+
+
+# LIST
+print([1, 2, 3, 4, 5])
+
+# TUPLE
+print({1, 2, 3, 4, 5})
+
+# DICTIONARY
+print({"name": "Alice", "age": 30, "city": "New York"})
---
+++ variable.py
@@ -0,0 +1,9 @@
+name = "Rashid"
+
+name = "Sabrina"
+name = 4
+
+a =2 
+b=5
+c=6
+print(a+b+c)
