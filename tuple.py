66e7b527d5d6c49ad8f6694c238e913379091581
Rashid004 <rashidansari3868038@gmail.com>
2026-06-11 11:12:40 +0530

    feat: add recursive factorial function and comment out movie input example

--- tuple.py
+++ tuple.py
@@ -1,12 +1,21 @@
-movies = []
+# movies = []
 
-movie1 = input("Enter movie 1")
-movie2 = input("Enter movie 2")
-movie3 = input("Enter movie 3")
+# movie1 = input("Enter movie 1")
+# movie2 = input("Enter movie 2")
+# movie3 = input("Enter movie 3")
 
 
-movies.append(movie1)
-movies.append(movie2)
-movies.append(movie3)
+# movies.append(movie1)
+# movies.append(movie2)
+# movies.append(movie3)
 
-print(movies)
+# print(movies)
+
+
+# print(movies.copy())
+
+
+# grade = ("A", "B", "B", "D", "E", "C", "D", "F")
+
+# sort = sorted(grade)
+# print(sort)
